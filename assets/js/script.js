91b5053c71967f85bc3eac0021bcfda9cbab9d28
reececilon <reecethat@gmail.com>
2021-12-29 22:31:35 +0000

    Error check

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,17 +1,30 @@
-const API_KEY = "S2piCSGFDG64hDPfdSz7DxHRL-0";
+const API_KEY = "S2piCSGFDG64hDPfdSz7DxHRL-0123";
 const API_URL = "https://ci-jshint.herokuapp.com/api";
 const resultsModal = new bootstrap.Modal(document.getElementById("resultsModal"));
 
-document.getElementsById("status").addEventListener("click", e => getStatus(e));
+document.getElementById("status").addEventListener("click", e => getStatus(e));
 
 async function getStatus(e) {
-    const queryString = `${API_URL}?API_KEY=${API_KEY}`;
+
+    const queryString = `${API_URL}?api_key=${API_KEY}`;
 
     const response = await fetch(queryString);
 
     const data = await response.json();
 
     if (response.ok) {
-        console.log(data);
+        displayStatus(data);
+    } else {
+        throw new Error(data.error)
     }
+
+}
+
+function displayStatus(data) {
+    let heading = "API key status";
+    let results = `<div>Your key is valid until</div>`;
+
+    results += `<div class = "key-status">${data.expiry}</div>`;
+
+    resultsModal.show();
 }
