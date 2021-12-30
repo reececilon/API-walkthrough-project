6db3cbf665f6cdf761f645931b67435ea988f4c9
reececilon <reecethat@gmail.com>
2021-12-30 14:04:27 +0000

    Add options string

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,8 +1,68 @@
-const API_KEY = "S2piCSGFDG64hDPfdSz7DxHRL-0123";
+const API_KEY = "S2piCSGFDG64hDPfdSz7DxHRL-0";
 const API_URL = "https://ci-jshint.herokuapp.com/api";
 const resultsModal = new bootstrap.Modal(document.getElementById("resultsModal"));
 
 document.getElementById("status").addEventListener("click", e => getStatus(e));
+document.getElementById("submit").addEventListener("click", e => postForm(e));
+
+function processOptions(form) {
+    let optArray = [];
+
+    for (let entry of form.entries()) {
+        if (entry[0] === options) {
+            optArray.push(entry[1]);
+        }
+    }
+    form.delete("options");
+
+    form.append("options", optArray.join());
+
+    return form;
+
+}
+
+async function postForm(e) {
+    const form = processOptions(new FormData(document.getElementById("checksform")));
+
+    for (let entry of form.entries()) {
+        console.log(entry);
+    }
+
+    const response = await fetch(API_URL, {
+        method: "POST",
+        headers: {
+            "Authorization": API_KEY,
+        },
+        body: form,
+    });
+
+    const data = await response.json();
+
+    if(response.ok) {
+        displayErrors(data);
+    } else {
+        throw new Error(data.error);
+    }
+}
+
+function displayErrors(data) {
+    let heading = `JSHint Results for ${data.file}`;
+
+    if (data.total_errors === 0) {
+        results = `<div class="no_errors">No errors reported</div>`
+    } else {
+        results = `<div>Total Erros: <span class="error_count">${data.total_errors}</span></div>`
+        for (let error of data.error_list) {
+            results += `<div>At line <span class="line">${error.line}</span>, `;
+            results += `column <span class="column">${error.col}</span></div>`;
+            results += `<div class="error">${error.error}</div>`;
+        }
+    }
+    document.getElementById("resultsModalTitle").innerText = heading;
+    document.getElementById("results-content").innerHTML = results;
+
+    resultsModal.show();
+}
 
 async function getStatus(e) {
 
@@ -23,8 +83,11 @@ async function getStatus(e) {
 function displayStatus(data) {
     let heading = "API key status";
     let results = `<div>Your key is valid until</div>`;
-
     results += `<div class = "key-status">${data.expiry}</div>`;
+
+
+    document.getElementById("resultsModalTitle").innerText = heading;
+    document.getElementById("results-content").innerHTML = results;
 
     resultsModal.show();
 }
